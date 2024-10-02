ab91287998344f50f246d91cc6c3343e97c4e980
6u6lik16 <serega22222223@mail.ru>
2024-10-02 22:05:23 +0400

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,4 +1,4 @@
-def send_email(message, recipient, sender="university.help@gmail.com"):
+def send_email(message, recipient,*, sender="university.help@gmail.com"):
     if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
         if "@" not in recipient or "@" not in sender:
             print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
@@ -18,12 +18,12 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
         return 0
 
 
-send_email("Привет, я Сергей", "university.help@gmail.com", "serega22222223@mail.ru")
+send_email("Привет, я Сергей", "university.help@gmail.com", sender="serega22222223@mail.ru")
 print("")
-send_email("Привет, Сергей, очень рады тебе", "serega22222223@mail.ru", "university.help@gmail.com")
+send_email("Привет, Сергей, очень рады тебе", "serega22222223@mail.ru")
 print("")
 # 1 Исключение - Отправка самому себе
-send_email("Я хейтер ", "Heiter@mail.ru", "Heiter@mail.ru")
+send_email("Я хейтер ", "Heiter@mail.ru", sender="Heiter@mail.ru")
 print("")
 # 2 Исключение - Некоректный Адрес почты
-send_email("Я хейтер ", "Heitermail.ru", "Heiter@mail")
+send_email("Я хейтер ", "Heitermail.ru", sender="Heiter@mail")
